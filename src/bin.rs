48518a44d8079b4f201c7a72eb23efdf3310c4db
Petteri Suckman <petteri.suckman@relexsolutions.com>
2024-09-26 19:29:55 +0300

    feat: enable basic logs, accessible through journalctl

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,19 @@
 use getserviceip::{run, IpInfo};
 use std::net::TcpListener; // Adjust the path to your lib.rs module
 use std::sync::{Arc, Mutex};
+use log::{trace, info, LevelFilter};
+use systemd_journal_logger::JournalLog;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Enable logging to systemd journal
+    JournalLog::new().unwrap().install().unwrap();
+    log::set_max_level(LevelFilter::Info);
+
+    trace!("Start server");
     // Bind to a specific address
     let listener = TcpListener::bind("0.0.0.0:8087")?;
+    info!("at address {:?}", &listener);
     let state = Arc::new(Mutex::new(Vec::<IpInfo>::new()));
     run(listener, state)?.await
 }
